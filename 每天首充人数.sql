81cbe9038fecc0c24769784102fbb6bd1de31b40
lijiadao <550920872@qq.com>
2019-01-05 10:34:15 +0800

    1111
    111

--- 每天首充人数.sql
+++ 每天首充人数.sql
@@ -18,7 +18,25 @@ INNER JOIN
 	) tt GROUP BY UserID   HAVING MIN(RecordTime) >=  AND MIN(RecordTime) < 
 ) b  ON a.UserID = b.UserID and a.RecordTime = b.RecordTime
 							
-							
+SELECT 
+a.UserID,
+a.PayAmount,
+a.RecordTime RecordTimeINT  
+FROM 
+(
+	SELECT ID,UserID,PayAmount,RecordTime FROM NewPlatform_Log.RechargePlatformOrder20181231235959 WHERE PayStatus=1 and RecordTime >= 1546531200 and RecordTime < 1546617600
+	UNION ALL
+	SELECT ID,UserID,PayAmount,RecordTime FROM NewPlatform_Log.RechargePlatformOrder20191231235959 WHERE PayStatus=1 and RecordTime >= 1546531200 and RecordTime < 1546617600
+) a 
+INNER JOIN 
+(
+    SELECT UserID,MIN(ID) ID  FROM 
+		(
+			 SELECT UserID,MIN(RecordTime) RecordTime,MIN(ID) ID  FROM NewPlatform_Log.RechargePlatformOrder20181231235959 WHERE PayStatus=1  GROUP BY UserID
+			 UNION ALL
+			 SELECT UserID,MIN(RecordTime) RecordTime,MIN(ID) ID  FROM NewPlatform_Log.RechargePlatformOrder20191231235959 WHERE PayStatus=1  GROUP BY UserID
+		) tt GROUP BY UserID   HAVING MIN(RecordTime) >= 1546531200  AND MIN(RecordTime) < 1546617600
+) b  ON a.UserID = b.UserID and a.ID = b.ID							
 
 							
 							
